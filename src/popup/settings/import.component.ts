b6aee39ee0a55a60b4b8f5878ce90a75b38ba86b
wuguyannian <wuguyannian@163.com>
2020-01-30 22:06:43 +0800

    import check

--- src/popup/settings/import.component.ts
+++ src/popup/settings/import.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { 
+    Component,
+    EventEmitter,
+    Output,} from '@angular/core';
 import { Router } from '@angular/router';
 
 import { CryptoService } from 'jslib/abstractions/crypto.service';
@@ -6,22 +9,68 @@ import { EventService } from 'jslib/abstractions/event.service';
 import { ExportService } from 'jslib/abstractions/export.service';
 import { I18nService } from 'jslib/abstractions/i18n.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
+import { EventType } from 'jslib/enums/eventType';
 
 @Component({
     selector: 'app-import',
     templateUrl: 'import.component.html',
 })
 export class ImportComponent  {
-    constructor(cryptoService: CryptoService, i18nService: I18nService,
-        platformUtilsService: PlatformUtilsService, exportService: ExportService,
-        eventService: EventService, private router: Router) {
+    @Output() onSaved = new EventEmitter();
+
+    formPromise: Promise<string>;
+    masterPassword: string;
+    format: 'json' | 'csv' = 'json';
+    showPassword = false;
+    selectFile: File = null;
+
+    constructor(
+        protected cryptoService: CryptoService, 
+        protected i18nService: I18nService,
+        protected platformUtilsService: PlatformUtilsService, 
+        protected exportService: ExportService,
+        protected eventService: EventService, private router: Router) {
     }
 
     protected saved() {
+        this.onSaved.emit();
         this.router.navigate(['/tabs/settings']);
     }
 
     onFileSelected(file:File){
-        console.log(file);
+        this.selectFile = file;
+    }
+
+    async submit() {
+        if (this.masterPassword == null || this.masterPassword === '') {
+            this.platformUtilsService.showToast('error', this.i18nService.t('errorOccurred'),
+                this.i18nService.t('invalidMasterPassword'));
+            return;
+        }
+        if (this.selectFile == null) {
+            this.platformUtilsService.showToast('error', this.i18nService.t('errorOccurred'),
+                this.i18nService.t('selectFile'));
+            return;
+        }
+
+        const keyHash = await this.cryptoService.hashPassword(this.masterPassword, null);
+        const storedKeyHash = await this.cryptoService.getKeyHash();
+        if (storedKeyHash != null && keyHash != null && storedKeyHash === keyHash) {
+            try {
+                // this.formPromise = this.getExportData();
+                // const data = await this.formPromise;
+                this.platformUtilsService.eventTrack('Imported Data');
+                this.saved();
+                await this.collectEvent();
+            } catch { }
+        } else {
+            this.platformUtilsService.showToast('error', this.i18nService.t('errorOccurred'),
+                this.i18nService.t('invalidMasterPassword'));
+        }
+    }
+
+    
+    protected async collectEvent(): Promise<any> {
+        await this.eventService.collect(EventType.User_ClientImportedVault);
     }
 }
